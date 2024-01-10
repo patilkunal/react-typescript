15b55953f8fab6827534cc36304984a7043cde97
Kunal Patil <kunalpatil@yahoo.com>
2024-01-09 21:44:07 -0500

    Working login form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,32 +1,100 @@
 import * as React from 'react';
 import './Login.css';
+import { Button, Form } from 'react-bootstrap';
 
 export interface ILoginProps {
 }
 
 const LoginComponent: React.FunctionComponent<ILoginProps> =  (props: ILoginProps) => {
+    const [formData, setFormData] = React.useState({
+        username:'',
+        email: '',
+        password: ''
+    });
+
+    const [validated, setValidated] = React.useState(false);
+
+    const [errors, setErrors] = React.useState({username:'', email:'', password:''});
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        //console.log(e);
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        const loginform = e.currentTarget as HTMLFormElement;
+        if(loginform.checkValidity() === false){
+
+        }
+        e.preventDefault();
+        const valErrors = {username:'', email: '', password: ''};
+        if(!formData.email.trim()) {
+            valErrors.email = 'Email is required';
+        } else if(! /\S+@\S+\.\S+/.test(formData.email)) {
+            valErrors.email = 'Email is not valid';
+        }
+        if(!formData.username.trim()) {
+            valErrors.username = 'Username is required';
+        } else if(! /\S+@\S+\.\S+/.test(formData.username)) {
+            valErrors.username = 'Username is not valid';
+        }
+        if(!formData.password.trim()) {
+            valErrors.password = 'Password is required';
+        }
+        setErrors(valErrors);
+        setValidated(true);
+    }
+
     return (
         <div className='wrapper d-flex align-items-center justify-content-center mt-20'>
             <div className='login'>
                 <h2 className='mb-3'>Login Form</h2>
+                <div>
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                    <Form.Group controlId='username'>
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control  required name="username" onChange={handleChange} isInvalid={!!errors.username}
+                        type='text' value={formData.username} placeholder='Enter username'></Form.Control>
+                        <Form.Control.Feedback type='invalid'>{errors.username}</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group controlId='password'>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control required 
+                        name="password" value={formData.password} onChange={handleChange} 
+                        isInvalid={!!errors.password} type='password' placeholder='Enter password'></Form.Control>
+                        <Form.Control.Feedback type='invalid'>{errors.password}</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group controlId='submit'>
+                        <Button type='submit' onClick={handleSubmit} variant='success' className='mt-2 w-100'>Login</Button>
+                    </Form.Group>
+                </Form>
+                </div>
+                {/*
+                <div>
                 <form className='needs-validation'>
-                    {/* add was-validated after the form is validated */}
-                    <div className='form-group mb-2'>
+                    <div className={`form-group ${errors.email && 'was-validated'} mb-2`}>
                         <label htmlFor='email' className='form-label'>Email Address</label> 
-                        <input type="email" className='form-control' required></input> 
-                        <div className="invalid-feedback">Please Enter you email</div>
+                        <input onChange={handleChange} type="email" name='email' id='email'  className='form-control'></input> 
+                        {errors.email &&
+                        <div className="invalid-feedback">errors.email</div>}
                     </div>
                     <div className='form-group mb-2'>
                         <label htmlFor='password' className='form-label'>Password</label>
-                        <input type='password' id='password' className='form-control' required></input>
+                        <input onChange={handleChange} type='password' name='password' id='password' className='form-control' required></input>
                     </div>
                     <div className='form-group form-check mb-2'>
-                        <input type='checkbox' id='rememberme' className='form-check-input'></input>
+                        <input type='checkbox' id='rememberme' name='rememberme' className='form-check-input'></input>
                         <label htmlFor='rememberme' className='form-check-label'>Remember Me</label>
                     </div>
-                    <button type="submit" className='btn btn-success mt-2 w-100'>Login</button>
+                    <button onSubmit={handleSubmit} type="submit" className='btn btn-success mt-2 w-100'>Login</button>
                 </form>
-            </div>
+                </div>
+                */}
+                </div>
         </div>
     );
   }
