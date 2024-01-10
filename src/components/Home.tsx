c6660a09ec30573f405b2002756cb8fc28e88a7d
Kunal Patil <kunalpatil@yahoo.com>
2024-01-09 20:20:49 -0500

    Example of collapsable element

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import Button from 'react-bootstrap/esm/Button';
+import Collapse from 'react-bootstrap/esm/Collapse';
 import { Outlet } from 'react-router-dom';
 
 export interface IHomeComponentProps {
@@ -5,10 +8,26 @@ export interface IHomeComponentProps {
 
 export function HomeComponent (props: IHomeComponentProps) {
     console.log(JSON.stringify(props));
+    const [open, setOpen] = React.useState(false);
   return (
     <div>
       THIS IS A HOME COMPONENT<br></br>
       Inside the <Outlet/> Outlet
+      <hr/>
+      <Button
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+      >
+        click
+      </Button>
+      <Collapse in={open}>
+        <div id="example-collapse-text">
+          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+          terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+          labore wes anderson cred nesciunt sapiente ea proident.
+        </div>
+      </Collapse>
     </div>
   );
 }
