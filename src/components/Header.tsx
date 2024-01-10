69c3185b15f58dd5d0f7843a9a2e63ccaadf30b3
Kunal Patil <kunalpatil@yahoo.com>
2024-01-09 20:20:01 -0500

    Fix navbar responsive issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 interface INavProps {
@@ -7,26 +7,15 @@ interface INavProps {
 
 const NavComponent: React.FunctionComponent<INavProps> = (props) => {
   return (
-    <Navbar bg="dark" variant='dark' expand="md" expanded={false} sticky='top'>
+    <Navbar collapseOnSelect bg="dark" variant='dark' expand="md" sticky='top'>
       <Container>
         <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link to="home" className='nav-link'>Home</Link>
-            <Link to="link" className='nav-link'>Link</Link>
-            <Link to="login" className='nav-link'>Login</Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link as={Link} to="home" eventKey='0'>Home</Nav.Link>
+            <Nav.Link as={Link} to="link" eventKey="1">Link</Nav.Link>
+            <Nav.Link as={Link} to="login" eventKey="2">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
